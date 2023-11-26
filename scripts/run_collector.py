f48f638299a3bfaa541fc80d51d448eaee2aea1e
Adam Torosyan <torosyan.adamu@gmail.com>
2023-11-26 16:56:08 +0300

    + параллелизация и замер времени работы скрипта

--- scripts/run_collector.py
+++ scripts/run_collector.py
@@ -1,6 +1,9 @@
 import sys
 
 sys.path = ["C:/stakan"] + sys.path
+import concurrent.futures
+import time
+
 from filesmeta.CommonMinion import CommonMinion
 from filesmeta.DocMinion import DocMinion
 from filesmeta.GRU import Gru
@@ -9,7 +12,7 @@ from filesmeta.PdfMinion import PDFMinion
 from staff.Collector import Collector
 
 
-def process_and_create_database(directory):
+def process_and_create_database_parallel(directory):
     c = Collector(directory)
 
     g = Gru()
@@ -23,15 +26,20 @@ def process_and_create_database(directory):
     g.add_minion(m4)
 
     file_paths = c.get_path()
-    metadata = g.process_files(file_paths)
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        metadata = list(executor.map(g.process_file, file_paths))
 
     c.make_database(metadata)
 
 
-def main():
+def main_parallel():
     directory = r"C:\Users\WarSa\OneDrive\Рабочий стол\www.msu.ru"
-    process_and_create_database(directory)
+    start_time = time.time()
+    process_and_create_database_parallel(directory)
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed Time: {elapsed_time} seconds")
 
 
 if __name__ == "__main__":
-    main()
+    main_parallel()
